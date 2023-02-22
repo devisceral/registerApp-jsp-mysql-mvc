5437672f95b7fe43e3a2386e51b54667663e76a7
devisceral <develmendes@gmail.com>
2023-02-22 18:49:15 -0300

    validation fields

--- src/main/java/register/controller/cadastrarProdutoController.java
+++ src/main/java/register/controller/cadastrarProdutoController.java
@@ -24,8 +24,7 @@ public class cadastrarProdutoController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("RECEBI A REQUISIÇÃO GET");
+		this.doPost(request, response);
 	}
 
 	/**
@@ -35,18 +34,43 @@ public class cadastrarProdutoController extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
-		String nome = request.getParameter("descricao");
-		int quantidade = Integer.parseInt(request.getParameter("quantidade"));
-		double preco = Double.parseDouble(request.getParameter("preco"));
-		boolean online = false;
+		String descricao = request.getParameter("descricao");
+		int quantidade;
+		double preco;
 		
-		if(request.getParameter("online") != null && request.getParameter("online").equals("on"))
-			online = true;
+		if(descricao != null && !descricao.isEmpty()&& request.getParameter("quantidade") != null
+			&& !request.getParameter("quantidade").isEmpty() && request.getParameter("preco") != null
+			&& !request.getParameter("preco").isEmpty()){
+			
+			quantidade = Integer.parseInt(request.getParameter("quantidade"));
+			
+			preco = Double.parseDouble(request.getParameter("preco"));
+			
+			System.out.println(descricao);
+			System.out.println(quantidade);
+			System.out.println(preco);
+			boolean online = false;
+			
+			if(request.getParameter("online") != null && request.getParameter("online").equals("on"))
+				online = true;
+			System.out.println(online);
+	
+		}
 		
-		System.out.println(nome);
-		System.out.println(quantidade);
-		System.out.println(preco);
-		System.out.println(online);
+		
+		
+//		String nome = request.getParameter("descricao");
+//		int quantidade = Integer.parseInt(request.getParameter("quantidade"));
+//		double preco = Double.parseDouble(request.getParameter("preco"));
+//		boolean online = false;
+//		
+//		if(request.getParameter("online") != null && request.getParameter("online").equals("on"))
+//			online = true;
+//		
+//		System.out.println(nome);
+//		System.out.println(quantidade);
+//		System.out.println(preco);
+//		System.out.println(online);
 	}
 
 }
